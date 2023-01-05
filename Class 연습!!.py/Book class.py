27f81013bf9ba1f3c61b23ae89df2364417d5fb9
Hun seok SUN <xodidxkq@naver.com>
2023-01-06 01:03:26 +0900

    add file

--- Class 연습!!.py/Book class.py
+++ Class 연습!!.py/Book class.py
@@ -1,23 +1,22 @@
 class Book:
 
-    def __init__(self, title, authors, publisher, isbn, price):
+    def __init__(self, title="", authors=[], publisher="", isbn="0", price=10.0):
+
         self.title = title
         self.authors = authors[:]
         self.publisher = publisher
         self.ISBN = isbn
         self.price = price
-    
+
     def __str__(self):
-        rep = " Title: {0}\n Authors: {1}\n Publisher: {2}\n ISBN: {3}\n Price: {4}".format(self.title, self.authors, self.publisher, self.ISBN, self.price)
+        rep = " Title: {0}\n Authors: {1}\n Publisher: {2}\n ISBN: {3}\n Price: {4}".format(
+            self.title, self.authors, self.publisher, self.ISBN, self.price)
         return rep
 
     def __eq__(self, other):
         return self.ISBN == other.ISBN
+        
 
     def num_authors(self):
         return len(self.authors)
-
-python_book = Book("Practical Programing",["A","B","C"],"SUN","12-1",300)
-print(python_book)
-
 
